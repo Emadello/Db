f19a59b795a513ee0e2f782042834961d42395a5
Emad Elshafie <emad_moh@outlook.com>
2020-02-17 08:41:06 +0200

    1.1.0 - Change con variable to function for better connections handling

--- src/Emadello/Db.php
+++ src/Emadello/Db.php
@@ -9,18 +9,26 @@ class Db {
 
   private $config;
   public $con;
+  protected $connected = false;
 
   function __construct() {
 
     $this->config = new Config();
 
-		// Database Connection
-		$stmt = "mysql:dbname=".$this->config::dbname.";host=".$this->config::dbhost.";port=".$this->config::dbport.";charset=utf8";
-		$this->con = new PDO($stmt, $this->config::dbuser, $this->config::dbpass);
-		$this->con->exec("set names utf8");
-		$this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 	}
+
+  public function con() {
+
+    if (!$this->connected) {
+      // Database Connection
+      $stmt = "mysql:dbname=".$this->config::dbname.";host=".$this->config::dbhost.";port=".$this->config::dbport.";charset=utf8";
+      $this->con = new PDO($stmt, $this->config::dbuser, $this->config::dbpass);
+      $this->con->exec("set names utf8");
+      $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->connected = true;
+      return $this->con;
+    } else return $this->con;
+  }
 
 }
 
